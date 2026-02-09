d79f7b258faf6cdbc41ea980f145f94bce819502
Hansueli Alder <hansueli.alder@batec.net>
2026-02-09 16:10:32 +0100

    Test2

--- OTD/App.axaml.cs
+++ OTD/App.axaml.cs
@@ -10,7 +10,7 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
-        Console.WriteLine("Test");
+        Console.WriteLine("Test1");
     }
 
     public override void OnFrameworkInitializationCompleted()
