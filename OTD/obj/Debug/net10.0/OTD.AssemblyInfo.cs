b1d68045714aa3d494c586b04681842e8a91d231
Hansueli Alder <hansueli.alder@batec.net>
2026-02-09 16:06:54 +0100

    Test

--- OTD/obj/Debug/net10.0/OTD.AssemblyInfo.cs
+++ OTD/obj/Debug/net10.0/OTD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e45990686753430bbf00ddf7b45d0d1ffd7b58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1009cdfbf603cc618cb216b1755e1ee81ecce124")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
