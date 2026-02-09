e311b7fb30cf9240d602a7d272dda1acffe89e1f
Siald20 <103524712+Siald20@users.noreply.github.com>
2026-02-09 16:22:10 +0100

    .

--- OTD/obj/Debug/net10.0/OTD.AssemblyInfo.cs
+++ OTD/obj/Debug/net10.0/OTD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2650f440026cfdee62bccdf601ccb276ab2b4eea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7591d25cead7515e753ef516ce1092d4555cf4d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
